ec9f41d566a70165f934d0578e4cbe4c8a736930
Dav <sotunboolamide@gmail.com>
2019-02-12 08:38:11 +0100

    Add files via upload

---
+++ Adapters/NewsListAdapters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Melgodas.Core.Model;
+
+namespace MelgodasAndroid.Adapters
+{
+    class NewsListAdapters : BaseAdapter<NewsModel>
+    {
+        List<NewsModel> newsList;
+        Activity activity;
+
+        public NewsListAdapters(Activity act, List<NewsModel> items)
+        {
+            activity = act;
+            newsList = items;
+        }
+        public override NewsModel this[int position] => newsList[position];
+
+        public override int Count => newsList.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var news = newsList[position];
+            if (convertView == null)
+            {
+                convertView = activity.LayoutInflater.Inflate(Resource.Layout.newsFeedRowView, null);
+            }
+            convertView.FindViewById<TextView>(Resource.Id.newsTitle).Text = news.Title;
+            convertView.FindViewById<TextView>(Resource.Id.newsDescription).Text = news.Description.Substring(0, 25) + "...";
+            //image
+
+            return convertView;
+        }
+    }
+}
---
+++ Adapters/priceListAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MelgodasAndroid.Model;
+
+namespace MelgodasAndroid.Adapters
+{
+    class priceListAdapter : BaseAdapter<Crop>
+    {
+        List<Crop> crops;
+        Activity activity;
+
+         public priceListAdapter(Activity context, List<Crop> cr)
+        {
+            activity = context;
+            crops = cr;
+        }
+
+        public override Crop this[int position] => crops[position];
+
+        public override int Count => crops.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var crop = crops[position];
+            if (convertView==null)
+            {
+                convertView = activity.LayoutInflater.Inflate(Resource.Layout.cropRowView, null);
+            }
+            convertView.FindViewById<TextView>(Resource.Id.cropName).Text = crop.Name;
+            convertView.FindViewById<TextView>(Resource.Id.cropPreviousPrice).Text = "N" + crop.PreviousPrice;
+            convertView.FindViewById<TextView>(Resource.Id.cropCurrentPrice).Text = "N" + crop.CurrentPrice;
+            convertView.FindViewById<TextView>(Resource.Id.netChange).Text = Math.Round(crop.NetChange,2) + "%";
+            //image
+
+            return convertView;
+        }
+    }
+}
